76529a4dd709891c49321829b0de9a625a08408b
saehwa95 <100126319+saehwa95@users.noreply.github.com>
2022-06-08 20:34:36 +0900

    Add files via upload

---
+++ todoList/src/Component/Todo.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import{addTodo, deleteTodo} from "../redux/reducer/reducer"
+
+const Todo = () => {
+
+const data =useSelector((state)=>state.todos)
+// console.log(data)
+
+const dispatch = useDispatch()
+
+const [text, setText] = useState("");
+
+
+const onChange = (event) => {
+  const { value } = event.target;
+  setText(value);
+}
+
+
+const plus = (e) => {
+  dispatch(addTodo(text))
+}
+
+const remove=(get)=>{
+  dispatch(deleteTodo(get))
+}
+
+
+const list = data.map((value,index) => {
+  return(
+    <div key={index}>
+    <h1><span>{value.id}{value.text}</span></h1>
+    {/* <button onClick={()=>update()}>수정</button> */}
+    <button onClick={()=>remove(value.id)}>삭제</button>
+    </div>
+  )
+})
+
+
+  return (
+    <>
+    <h1>Todo</h1>
+    <input type="text" onChange={onChange} value={text} />
+    <button onClick={plus}>추가</button>
+    {list}
+    </>
+  )
+}
+
+export default Todo;
---
+++ todoList/src/redux/reducer/reducer.js
@@ -0,0 +1,43 @@
+//액션 타입
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO = "DELETE_TODO"
+const GET_TODO = "GET_TODO"
+
+//액션 크리에이터
+export const addTodo = (payload) => {
+  return {type : ADD_TODO, payload}
+};
+
+export const deleteTodo = (payload) => {
+  return {type : DELETE_TODO, payload}
+};
+
+export const getTodo = (payload) => {
+  return {type : GET_TODO, payload}
+}
+
+//초기값
+const initialState = {
+  todos: [],
+};
+
+//리듀서
+const todos = (state = initialState, action) => {
+  switch(action.type) {
+        //왜 return () 아니고, {}?
+    case ADD_TODO: return {...state, todos:[...state.todos, {id:state.todos.length+1, text:action.payload}]}
+
+    case DELETE_TODO:
+      console.log(action)
+      // return() 아니고, {return 바로..~~ ?}
+    return{...state, todos : state.todos.filter((todo)=>{return todo.id !== action.payload}),
+    };
+
+    case GET_TODO: return{...state}
+
+    default:
+      return state;
+  }
+};
+
+export default todos;
